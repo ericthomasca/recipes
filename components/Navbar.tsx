61ea315ec3e9ed307fe740477fa9338657df1fc7
Eric Thomas <eric@ericthomas.ca>
2024-02-09 21:13:24 -0330

    update Links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,17 +4,14 @@ export default function Navbar() {
   return (
     <nav className='bg-white border-gray-200 dark:bg-gray-900'>
       <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-        <Link href='/'>
-          <a className='flex items-center space-x-3 rtl:space-x-reverse'>
-            <img
-              src='/logo.png'
-              className='h-8'
-              alt='Recipes Logo'
-            />
-            <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
-              Recipes
-            </span>
-          </a>
+        <Link
+          href='/'
+          className='flex items-center space-x-3 rtl:space-x-reverse'
+        >
+          <img src='/logo.png' className='h-8' alt='Recipes Logo' />
+          <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
+            Recipes
+          </span>
         </Link>
         <div className='flex md:order-2'>
           <button
@@ -123,24 +120,27 @@ export default function Navbar() {
           </div>
           <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
             <li>
-              <Link href='/'>
-                <a className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'>
-                  Home
-                </a>
+              <Link
+                href='/'
+                className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
+              >
+                Home
               </Link>
             </li>
             <li>
-              <Link href='/about'>
-                <a className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
-                  About
-                </a>
+              <Link
+                href='/about'
+                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+              >
+                About
               </Link>
             </li>
             <li>
-              <Link href='/services'>
-                <a className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
-                  Services
-                </a>
+              <Link
+                href='/services'
+                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+              >
+                Services
               </Link>
             </li>
           </ul>
