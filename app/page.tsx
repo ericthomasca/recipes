b0c5e22c001b3a7d5254130b1fd879e0b8c03a41
Eric Thomas <eric@ericthomas.ca>
2024-02-10 15:57:19 -0330

    replace img with Image

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import RecipeCard from '../components/RecipeCard';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+    <main className="flex flex-col items-center justify-between p-12 h-screen bg-gray-800">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-12">
         {recipes.map((recipe: Recipe) => (
           <RecipeCard key={recipe.id} recipe={recipe} />
         ))}
--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,5 +1,6 @@
-import Link from 'next/link';
-import { Recipe } from '../data';
+import Link from "next/link";
+import { Recipe } from "../data";
+import Image from "next/image";
 
 interface RecipeCardProps {
   recipe: Recipe;
@@ -8,10 +9,19 @@ interface RecipeCardProps {
 export default function RecipeCard({ recipe }: RecipeCardProps) {
   return (
     <Link href={`/recipes/${recipe.slug}`} legacyBehavior>
-      <a className="block max-w-xs rounded overflow-hidden shadow-lg bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300">
-        <img className="w-full" src={recipe.imageUrl} alt={recipe.title} />
-        <div className="p-4">
-          <div className="font-bold text-2xl mb-1">{recipe.title}</div>
+      <a className='block max-w-xs rounded overflow-hidden shadow-lg bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100 hover:shadow-xl transition duration-300'>
+        <div className='relative h-56'>
+          <Image
+            src={recipe.imageUrl}
+            alt={recipe.title}
+            layout='fill'
+            objectFit='cover'
+          />
+        </div>
+        <div className='p-4'>
+          <div className='font-bold text-xl mb-2 text-gray-800 dark:text-gray-100'>
+            {recipe.title}
+          </div>
         </div>
       </a>
     </Link>
