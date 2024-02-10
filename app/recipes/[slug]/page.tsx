a38c67cf91e5e67c3d885c314694fccdf456148a
Eric Thomas <eric@ericthomas.ca>
2024-02-10 16:04:30 -0330

    padding and color on slug page

--- app/recipes/[slug]/page.tsx
+++ app/recipes/[slug]/page.tsx
@@ -13,35 +13,35 @@ export default async function Page({ params }: { params: { slug: string } }) {
   }
 
   return (
-    <main className='container mx-auto px-4 py-8'>
-      <div className='max-w-4xl mx-auto'>
-        <h1 className='text-3xl font-bold mb-4'>{recipe.title}</h1>
+    <main className=" mx-auto px-4 py-8 bg-gray-800 min-h-screen">
+      <div className="max-w-4xl mx-auto w-full">
+        <h1 className="text-3xl font-bold mb-4 text-white">{recipe.title}</h1>
         <img
-          className='w-full rounded-lg mb-4'
+          className="w-full rounded-lg mb-4"
           src={recipe.imageUrl}
           alt={recipe.title}
         />
-        <div className='mb-4'>
-          <h2 className='text-xl font-bold mb-2'>Ingredients</h2>
-          <ul className='list-disc pl-5'>
+        <div className="mb-4 text-white">
+          <h2 className="text-xl font-bold mb-2">Ingredients</h2>
+          <ul className="list-disc pl-5">
             {recipe.ingredients.map((ingredient, index) => (
-              <li key={index} className='mb-1'>
+              <li key={index} className="mb-1">
                 {ingredient}
               </li>
             ))}
           </ul>
         </div>
-        <div className='mb-4'>
-          <h2 className='text-xl font-bold mb-2'>Instructions</h2>
-          <ol className='list-decimal pl-5'>
+        <div className="mb-4 text-white">
+          <h2 className="text-xl font-bold mb-2">Instructions</h2>
+          <ol className="list-decimal pl-5">
             {recipe.instructions.map((instruction, index) => (
-              <li key={index} className='mb-1'>
+              <li key={index} className="mb-1">
                 {instruction}
               </li>
             ))}
           </ol>
         </div>
-        <p className='text-lg'>
+        <p className="text-lg text-white">
           <strong>Yield:</strong> {recipe.yield}
         </p>
       </div>
