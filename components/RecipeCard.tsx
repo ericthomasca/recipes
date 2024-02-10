661eeee2000bbcbbd3a71e341dfed5dac9c8b51e
Eric Thomas <eric@ericthomas.ca>
2024-02-10 00:25:06 -0330

    back to working state

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Recipe } from '../data';
 
 interface RecipeCardProps {
@@ -6,7 +7,7 @@ interface RecipeCardProps {
 
 export default function RecipeCard({ recipe }: RecipeCardProps) {
   return (
-    <Link href={`/recipes/${recipe.slug}`}>
+    <Link href={`/recipes/${recipe.slug}`} legacyBehavior>
       <a className="block max-w-xs rounded overflow-hidden shadow-lg bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300">
         <img className="w-full" src={recipe.imageUrl} alt={recipe.title} />
         <div className="p-4">
